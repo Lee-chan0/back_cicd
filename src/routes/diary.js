e9b35975f27442b5e02cb9a26b7baa3635d9d9eb
Jhyun Y <yjhorion@gmail.com>
2023-12-11 11:30:18 +0900

    일기 수정부분 수정, 일기 추가부분에 이미지 업로드 기능 병합

--- src/routes/diary.js
+++ src/routes/diary.js
@@ -1,10 +1,15 @@
-import express from "express";
-import { prisma } from "../utils/prisma/index.js";
-import authMiddleware from "../middleware/auth.middleware.js";
-import { startOfDay, endOfDay } from "date-fns";
-import { utcToZonedTime } from "date-fns-tz";
+import express from 'express'
+import { prisma } from '../utils/prisma/index.js';
+import authMiddleware from '../middlewares/auth.middleware.js';
+import { startOfDay, endOfDay } from 'date-fns';
+import { utcToZonedTime, format } from 'date-fns-tz';
+import multer from 'multer'
+import multerS3 from 'multer-s3'
+import AWS from 'aws-sdk'
+import dotenv from "dotenv"
 
 const router = express.Router();
+dotenv.config();
 
 /* 작성한 글 상세조회(캘린더 클릭 포함) */
 router.get('/diary/detail/:diaryId', async (req, res, next) => {
@@ -22,75 +27,93 @@ router.get('/diary/detail/:diaryId', async (req, res, next) => {
 
 
 /* 오늘의 일기 작성 */
-router.post('/diary/posting', authMiddleware, async (req, res, next) => {
-  try {
+const s3 = new AWS.S3({
+    // process.env.S3_ACCESS_KEY
+    // process.env.S3_SECRET_ACCESS_KEY
+    accessKeyId: process.env.S3_ACCESS_KEY,
+    secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+    region: 'ap-northeast-2',
+  });
+
+const BUCKET_NAME = 'finaldrawings'
+
+const upload = multer({
+    storage: multerS3({
+      s3: s3,
+      bucket: BUCKET_NAME,
+      contentType: multerS3.AUTO_CONTENT_TYPE,
+      acl: 'public-read', // 접근 권한 설정 (public-read로 설정하면 URL로 접근 가능)
+      key: function (req, file, cb) {
+        cb(null, Date.now().toString() + '-' + file.originalname); // 파일 이름 설정
+      },
+    }),
+  });
+  
+  app.post('/diary/posting', authMiddleware, upload.single('picture'), async (req, res, next) => {
+    try {
       const { userId } = req.user;
-      const { EmotionStatus, content, image, isPublic } = req.body;
+      const { EmotionStatus, content } = req.body;
+      const imageBuffer = req.file.buffer;
+  
       const today = new Date();
       const timeZone = 'Asia/Seoul';
       const todaySeoulTime = utcToZonedTime(today, timeZone);
       const startOfToday = startOfDay(todaySeoulTime);
       const endOfToday = endOfDay(todaySeoulTime);
-
+  
       const diaryExists = await prisma.diaries.findFirst({
-          where: {
-              createdAt: {
-                  gte: startOfToday,
-                  lte: endOfToday
-              },
-              UserId: userId
-          }
+        where: {
+          createdAt: {
+            gte: startOfToday,
+            lte: endOfToday,
+          },
+          UserId: userId,
+        },
       });
-
+  
       if (diaryExists) {
-          return res.status(300).json({ message: "오늘은 이미 작성한 글이 있습니다. 수정하시겠습니까?" }); // front 에서 확인을 누를경우 router.patch('/diary/edit/:diaryId') 로 redirect
+        return res.status(300).json({ message: '오늘은 이미 작성한 글이 있습니다. 수정하시겠습니까?' });
       }
-
-      const diary = await prisma.diaries.create({
-          data: {
-              UserId: userId,
-              EmotionStatus,
-              content,
-              image,
-              isPublic,
-          }
+  
+      const params = {
+        Bucket: BUCKET_NAME,
+        Key: Date.now().toString() + '-' + req.file.originalname,
+        Body: imageBuffer,
+        ContentType: req.file.mimetype,
+        ACL: 'public-read',
+      };
+  
+      const s3Upload = await s3.upload(params).promise();
+  
+      const savedImage = await prisma.Images.create({
+        data: {
+          filename: req.file.originalname,
+          mimetype: req.file.mimetype,
+          // S3에 저장된 이미지의 URL을 사용하도록 수정
+          data: s3Upload.Location,
+        },
       });
-      return res.status(201).json({ message: "다이어리 등록 완료", data: diary });
-  } catch (error) {
+  
+      const diary = await prisma.diaries.create({
+        data: {
+          UserId: userId,
+          EmotionStatus,
+          content,
+          image: {
+            connect: {
+              imageId: savedImage.imageId,
+            },
+          },
+        },
+      });
+  
+      return res.status(201).json({ message: '다이어리 등록 완료', data: diary });
+    } catch (error) {
       return res.status(400).json({ error: error.message });
-  }
-});
+    }
+  });
 
-/* 일기 수정 */
-// router.patch('/diary/edit/:diaryId', authMiddleware, async (req, res, next) => {
-//   try {
-//       const { userId } = req.user;
-//       const { EmotionStatus, content, image, isPublic } = req.body;
-//       const { diaryId } = req.params;
-
-//       const diary = await prisma.diaries.findFirst({
-//           where: { diaryId: +diaryId }
-//       });
-
-//       if (diary.UserId !== userId) {
-//           return res.status(401).json({ message: "수정 권한이 없는 게시물입니다" });
-//       }
-
-//       if (!diary) {
-//           return res.status(400).json({ message: "수정하려는 일기가 존재하지 않습니다" });
-//       }
-
-//       await prisma.diaries.update({
-//           where: { diaryId: +diaryId },
-//           data: { content, image, EmotionStatus, isPublic }
-//       });
-//       return res.status(201).json({ message: "수정완료" });
-
-//   } catch (error) {
-//       return res.status(400).json({ error: error.message });
-//   }
-// });
-
+/* 일기수정 */
 router.patch('/diary/edit/:diaryId', authMiddleware, async (req, res, next) => {
     try {
       const { userId } = req.user;
@@ -109,20 +132,16 @@ router.patch('/diary/edit/:diaryId', authMiddleware, async (req, res, next) => {
         return res.status(401).json({ message: "수정 권한이 없는 게시물입니다" });
       }
   
-      // 이미지 업데이트를 위한 처리
-      const { imageId } = req.body; // 업데이트할 이미지의 ID를 받아옵니다.
-  
-      // 이미지 ID가 제공되었고, 이미지가 존재하는지 확인
-      if (imageId) {
-        const imageExists = await prisma.Images.findFirst({
-          where: { imageId: +imageId }
+      // 이미지가 수정되는 경우에만 처리
+      if (req.file) {
+        const savedImage = await prisma.Images.create({
+          data: {
+            filename: req.file.originalname,
+            mimetype: req.file.mimetype,
+            data: `https://${req.file.bucket}.s3.amazonaws.com/${req.file.key}`,
+          },
         });
   
-        if (!imageExists) {
-          return res.status(400).json({ message: "업데이트하려는 이미지가 존재하지 않습니다" });
-        }
-  
-        // 기존 일기와 이미지 연결 업데이트
         await prisma.diaries.update({
           where: { diaryId: +diaryId },
           data: {
@@ -131,20 +150,20 @@ router.patch('/diary/edit/:diaryId', authMiddleware, async (req, res, next) => {
             isPublic,
             image: {
               connect: {
-                imageId: +imageId // 이미지 ID로 기존 이미지와 연결합니다.
-              }
-            }
-          }
+                imageId: savedImage.imageId,
+              },
+            },
+          },
         });
       } else {
-        // 이미지 ID가 없는 경우 이미지 필드를 업데이트하지 않습니다.
+        // 이미지가 수정되지 않은 경우에는 이미지 필드를 업데이트하지 않음
         await prisma.diaries.update({
           where: { diaryId: +diaryId },
           data: {
             content,
             EmotionStatus,
-            isPublic
-          }
+            isPublic,
+          },
         });
       }
   
@@ -154,6 +173,7 @@ router.patch('/diary/edit/:diaryId', authMiddleware, async (req, res, next) => {
       return res.status(400).json({ error: error.message });
     }
   });
+  
   
 
 export default router;
