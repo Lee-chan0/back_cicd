470d6a420af82b922b4ff14f5c3dd59b8aa64b63
Lee-chan0 <yab0403@kakao.com>
2023-12-13 13:18:47 +0900

    google 4

--- src/Oauth/google.login.js
+++ src/Oauth/google.login.js
@@ -7,25 +7,14 @@ import authMiddleware from "../middleware/auth.middleware.js";
 import redis from "ioredis";
 import axios from 'axios';
 
-
 const router = express.Router();
 dotenv.config();
 
 const GOOGLE_TOKEN_URL = 'https://oauth2.googleapis.com/token'
 const GOOGLE_USERINFO_URL = 'https://www.googleapis.com/oauth2/v2/userinfo'
 
-// router.get('/googlelogin', (req, res, next) => {
-//     let url = `https://accounts.google.com/o/oauth2/v2/auth`;
-
-//     url=url +`?client_id=${process.env.GOOGLE_CLIENT_ID}`
-//     url=url +`&redirect_uri=${process.env.REACT_APP_GOOGLE_AUTH_REDIRECT_URI}`
-//     url=url +`&response_type=code`
-//     url=url +`&scope=email profile`
-
-//     res.redirect(url);
-// });
-
 router.post('/google/callback', async(req, res, next) => {
+    const key = process.env.SECRET_KEY;
     const {code} = req.body;
     console.log('code=======', code);
     const resp = await axios.post(GOOGLE_TOKEN_URL, {
@@ -35,39 +24,44 @@ router.post('/google/callback', async(req, res, next) => {
         redirect_uri : process.env.REACT_APP_GOOGLE_AUTH_REDIRECT_URI,
         grant_type : `authorization_code`
     });
-    console.log('-=--------------------------------------------------------------')
     const resp2 = await axios.get(GOOGLE_USERINFO_URL, {
         headers : {
             authorization: `Bearer ${resp.data.access_token}`
         }
     });
 
-    console.log(resp2.data);
+    const findUser = await prisma.users.findFirst({where : {email : resp2.data.email}});
+    if(findUser){
+        const accesstoken = jwt.sign({userId : findUser.userId}, key, {expiresIn : '30m'});
+        const refreshtoken = jwt.sign({userId : findUser.userId}, key, {expiresIn : '7d'});
 
-    const {name, email, picture} = resp2.data;
-    console.log(name)
-    console.log(email)
-    console.log(picture)
+        const access_token_time = jwt.verify(accesstoken, key);
 
-    // const findUser = await prisma.users.findFirst({where : {email : email}});
-    // if(findUser){
-    //     const token = jwt.sign({userId : findUser.userId}, process.env.SECRET_KEY);
-    //     res.setHeader('authorization', token);
-    //     return res.status(200).json({message : "로그인에 성공했습니다."})
-    // }
+        res.setHeader('Authorization', accesstoken);
+        res.setHeader('Refreshtoken', refreshtoken);
+        res.setHeader('Expiredtime', access_token_time.exp);
 
+        return res.status(201).json({message : "로그인 성공"});
+    }else {
+        const createUser = await prisma.users.create({
+            data : {
+                email : resp2.data.email,
+                password : '124okOK1234',
+                username : resp2.data.name,
+                profileImg : resp2.data.picture
+            }
+        })
+        const accesstoken = jwt.sign({userId : createUser.userId}, key, {expiresIn : '30m'});
+        const refreshtoken = jwt.sign({userId : createUser.userId}, key, {expiresIn : '7d'});
 
-    // const user = await prisma.users.create({
-    //     data : {
-    //         username : name,
-    //         email : email,
-    //         profileImg : picture
-    //     }
-    // })
+        const access_token_time = jwt.verify(accesstoken, key);
 
-    // const token = jwt.sign({userId : user.userId}, process.env.SECRET_KEY);
-    // res.setHeader('authorization', token);
-    return res.status(201).json({message : "회원가입 성공"});
+        res.setHeader('Authorization', accesstoken);
+        res.setHeader('Refreshtoken', refreshtoken);
+        res.setHeader('Expiredtime', access_token_time.exp);
+
+        return res.status(201).json({message : "회원가입 성공"})
+    }
 })
 
 export default router;
