23dbda4c6cf76411983d2969c350b017e90689b2
Jhyun Y <yjhorion@gmail.com>
2023-12-13 16:06:19 +0900

    diaries 수정

--- src/routes/diary.js
+++ src/routes/diary.js
@@ -6,15 +6,16 @@ import { utcToZonedTime } from 'date-fns-tz';
 import { upload } from '../middleware/S3.upload/multer.js'
 
 
+
 const router = express.Router();
 
 
 router.post('/diary/posting', authMiddleware, upload.single('image'), async (req, res, next) => {
   try {
     const { userId } = req.user;
-    const { EmotionStatus, content } = req.body;
+    const { EmotionStatus, content, isPublic } = req.body;
 
-    const imageUrl = req.file.location;
+    const  imageUrl = req.file.location
 
     const today = new Date();
     const timeZone = 'Asia/Seoul';
@@ -39,9 +40,10 @@ router.post('/diary/posting', authMiddleware, upload.single('image'), async (req
     const savedDiary = await prisma.diaries.create({
       data: {
         UserId: userId,
-        EmotionStatus,
+        EmotionStatus : +EmotionStatus,
         content,
         image: imageUrl,
+        isPublic: Boolean(isPublic)
       },  
     });
 
@@ -52,65 +54,65 @@ router.post('/diary/posting', authMiddleware, upload.single('image'), async (req
 });
  
 /* 일기수정 */
-router.patch('/diary/edit/:diaryId', authMiddleware, async (req, res, next) => {
-    try {
-      const { userId } = req.user;
-      const { EmotionStatus, content, isPublic } = req.body;
-      const { diaryId } = req.params;
+// router.patch('/diary/edit/:diaryId', authMiddleware, async (req, res, next) => {
+//     try {
+//       const { userId } = req.user;
+//       const { EmotionStatus, content, isPublic } = req.body;
+//       const { diaryId } = req.params;
   
-      const diary = await prisma.diaries.findFirst({
-        where: { diaryId: +diaryId }
-      });
+//       const diary = await prisma.diaries.findFirst({
+//         where: { diaryId: +diaryId }
+//       });
   
-      if (!diary) {
-        return res.status(400).json({ message: "수정하려는 일기가 존재하지 않습니다" });
-      }
+//       if (!diary) {
+//         return res.status(400).json({ message: "수정하려는 일기가 존재하지 않습니다" });
+//       }
   
-      if (diary.UserId !== userId) {
-        return res.status(401).json({ message: "수정 권한이 없는 게시물입니다" });
-      }
+//       if (diary.UserId !== userId) {
+//         return res.status(401).json({ message: "수정 권한이 없는 게시물입니다" });
+//       }
   
-      // 이미지가 수정되는 경우에만 처리
-      if (req.file) {
-        const savedImage = await prisma.Images.create({
-          data: {
-            filename: req.file.originalname,
-            mimetype: req.file.mimetype,
-            data: `https://${req.file.bucket}.s3.amazonaws.com/${req.file.key}`,
-          },
-        });
+//       // 이미지가 수정되는 경우에만 처리
+//       if (req.file) {
+//         const savedImage = await prisma.Images.create({
+//           data: {
+//             filename: req.file.originalname,
+//             mimetype: req.file.mimetype,
+//             data: `https://${req.file.bucket}.s3.amazonaws.com/${req.file.key}`,
+//           },
+//         });
   
-        await prisma.diaries.update({
-          where: { diaryId: +diaryId },
-          data: {
-            content,
-            EmotionStatus,
-            isPublic,
-            image: {
-              connect: {
-                imageId: savedImage.imageId,
-              },
-            },
-          },
-        });
-      } else {
-        // 이미지가 수정되지 않은 경우에는 이미지 필드를 업데이트하지 않음
-        await prisma.diaries.update({
-          where: { diaryId: +diaryId },
-          data: {
-            content,
-            EmotionStatus,
-            isPublic,
-          },
-        });
-      }
+//         await prisma.diaries.update({
+//           where: { diaryId: +diaryId },
+//           data: {
+//             content,
+//             EmotionStatus : +EmotionStatus,
+//             isPublic,
+//             image: {
+//               connect: {
+//                 imageId: savedImage.imageId,
+//               },
+//             },
+//           },
+//         });
+//       } else {
+//         // 이미지가 수정되지 않은 경우에는 이미지 필드를 업데이트하지 않음
+//         await prisma.diaries.update({
+//           where: { diaryId: +diaryId },
+//           data: {
+//             content,
+//             EmotionStatus,
+//             isPublic,
+//           },
+//         });
+//       }
   
-      return res.status(201).json({ message: "수정완료" });
+//       return res.status(201).json({ message: "수정완료" });
   
-    } catch (error) {
-      return res.status(400).json({ error: error.message });
-    }
-  });
+//     } catch (error) {
+//       return res.status(400).json({ error: error.message });
+//     }
+//   });
   
   
 
