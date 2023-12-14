f3cfe0656451584076b9b9cfef6a61d622708ccb
Lee-chan0 <yab0403@kakao.com>
2023-12-14 20:39:30 +0900

    redis

--- src/redis/redis.js
+++ src/redis/redis.js
@@ -4,9 +4,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const client = new redis(
-  // {
-  //   host : process.env.REDIS_HOST,
-  //   port : process.env.REDIS_PORT,
-  //   password : process.env.REDIS_PASSWORD
-  // }
-  );
+  {
+    host : process.env.REDIS_HOST,
+    port : process.env.REDIS_PORT,
+    password : process.env.REDIS_PASSWORD
+  }
+);
