982e12cc908ea49d73058071aa6767c8ca208b95
Lee-chan0 <yab0403@kakao.com>
2023-12-27 05:02:34 +0900

    docker TEST

--- healthcheck_restart.sh
+++ healthcheck_restart.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-# 대상 서비스 이름 설정
-SERVICE_NAME="blue"  # 'blue' 또는 'green'으로 설정
+SERVICE_NAME="blue"  
 
-# 주기적으로 상태 확인
+
 while true; do
-    # Docker Compose를 사용하여 컨테이너 ID를 가져옴
+
     CONTAINER_ID=$(docker-compose ps -q $SERVICE_NAME)
 
-    # 컨테이너 상태 확인
     if [ ! -z "$CONTAINER_ID" ]; then
         SERVICE_STATUS=$(docker inspect --format='{{.State.Health.Status}}' $CONTAINER_ID)
 
-        # 상태에 따른 조치
         if [ "$SERVICE_STATUS" == "unhealthy" ]; then
             echo "Service $SERVICE_NAME is unhealthy. Restarting..."
             docker-compose restart $SERVICE_NAME
