3beee938c184e2718f8ead927984ecc561e92a95
Lee-chan0 <yab0403@kakao.com>
2023-12-30 03:32:41 +0900

    error edit

--- src/middleware/error-handling.middleware.js
+++ src/middleware/error-handling.middleware.js
@@ -3,5 +3,5 @@ export default function (err, req, res, next) {
   if(err.name === "TokenExpiredError"){
     return res.status(419).json({message: "토큰이 만료 되었습니다."})
   }
-  res.status(500).json({ errorMessage: '서버 내부 에러가 발생했습니다.' });
+  return res.status(500).json({ errorMessage: '서버 내부 에러가 발생했습니다.' });
 }
