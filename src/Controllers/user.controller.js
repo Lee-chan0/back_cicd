13204968ed86b329811ac75bd4eb48158e104829
Jhyun Y <yjhorion@gmail.com>
2023-12-30 12:51:03 +0900

    mongoose error처리

--- src/Controllers/user.controller.js
+++ src/Controllers/user.controller.js
@@ -4,23 +4,29 @@ const userController = {}
 
 userController.saveUser=async(userName, sid)=>{
     // 이미 있는 유저인지 확인
-    let user = await UserModel.findOne({ name: userName });
-    // 없다면 새로 유저 정보 만들기
-    if(!user){
-        user = new UserModel({
-            name:userName,
-            token:sid,
-            online:true,
-        })
+    try {
+        
+        let user = await UserModel.findOne({ name: userName });
+        // 없다면 새로 유저 정보 만들기
+        if(!user){
+            user = new UserModel({
+                name:userName,
+                token:sid,
+                online:true,
+            })
+        }
+        // 이미 있는 유저라면 연결 정보 token값만 바꿔주자
+        if (user) {
+            user.token = sid
+            user.online=true
+        }
+        
+        await user.save();
+        return user;
+    } catch (err) {
+        console.error(err);
+        throw new Error(err.message)
     }
-    // 이미 있는 유저라면 연결 정보 token값만 바꿔주자
-    if (user) {
-        user.token = sid
-        user.online=true
-    }
-    
-    await user.save();
-    return user;
 }
 
 userController.checkUser = async(sid) => {
